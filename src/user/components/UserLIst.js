b2474ff4fc26979b2e6ad2611eebeba2dcfbbf7c
JUSTGARCIA5 <JUSTGARCIA5@GMAIL.COM>
2022-08-07 21:29:30 -0700

    Updated logic for place count in UserList compoenent

--- src/user/components/UserLIst.js
+++ src/user/components/UserLIst.js
@@ -31,7 +31,7 @@ const UserList = props => {
           id={user.id}
           image={user.image}
           name={user.name}
-          placeCount={user.places}
+          placeCount={user.places.length - 1}
         />
       ))}
     </ul>
